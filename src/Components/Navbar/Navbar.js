0438002cf8d28e118ae24da0d9806f43854ec4a0
bae151 <priyalsoni46@gmail.com>
2023-06-06 23:33:55 +0530

    subNavbar Work

--- src/Components/Navbar/Navbar.js
+++ src/Components/Navbar/Navbar.js
@@ -1,24 +1,37 @@
 import * as React from 'react';
-// import { styled, alpha } from '@mui/material/styles';
+
+
 import AppBar from '@mui/material/AppBar';
 import Box from '@mui/material/Box';
 import Toolbar from '@mui/material/Toolbar';
-import {MdOutlineNotificationsNone} from 'react-icons/md';
-import {RiQuestionLine} from 'react-icons/ri';
+import { MdOutlineNotificationsNone } from 'react-icons/md';
+import { RiQuestionLine } from 'react-icons/ri';
 import { FaTrello } from 'react-icons/fa'
 import { FaTh } from 'react-icons/fa'
-import {MdBrightness6} from 'react-icons/md'
-// import IconButton from '@mui/material/IconButton';
-// import Typography from '@mui/material/Typography';
+import { MdBrightness6 } from 'react-icons/md'
+
 import InputBase from '@mui/material/InputBase';
 import { styled, alpha } from '@mui/material/styles';
 import SearchIcon from '@mui/icons-material/Search';
 import styles from './Navbar.module.css';
 
+import { ImHome } from 'react-icons/im'
+
+import { HiUsers } from 'react-icons/hi'
+
+import {FiStar} from 'react-icons/fi'
+import {SiPowerbi} from 'react-icons/si';
+import {AiTwotoneThunderbolt} from 'react-icons/ai'
+import {BiFilter} from 'react-icons/bi'
+import {RiUserAddLine} from 'react-icons/ri'
+import {BsThreeDots} from 'react-icons/bs'
+
+
 
 
 const Search = styled('div')(({ theme }) => ({
     position: 'relative',
+
     borderRadius: theme.shape.borderRadius,
     backgroundColor: alpha(theme.palette.common.white, 0.15),
     '&:hover': {
@@ -40,6 +53,7 @@ const SearchIconWrapper = styled('div')(({ theme }) => ({
     display: 'flex',
     alignItems: 'center',
     justifyContent: 'center',
+
 }));
 
 const StyledInputBase = styled(InputBase)(({ theme }) => ({
@@ -48,9 +62,12 @@ const StyledInputBase = styled(InputBase)(({ theme }) => ({
         padding: theme.spacing(1, 1, 1, 0),
         // vertical padding + font size from searchIcon
         paddingLeft: `calc(1em + ${theme.spacing(4)})`,
+
         transition: theme.transitions.create('width'),
         width: '100%',
+
         [theme.breakpoints.up('sm')]: {
+
             width: '12ch',
             '&:focus': {
                 width: '20ch',
@@ -62,39 +79,81 @@ const StyledInputBase = styled(InputBase)(({ theme }) => ({
 
 
 
+
+
+
 export default function SearchAppBar() {
     return (
-        <Box sx={{ flexGrow: 1 }}>
-            <AppBar position="static" style={{ backgroundColor: 'black', height: '8vh' }}>
-                <Toolbar >
-                    <FaTh />
+
+        
+        <div>
+            <Box sx={{ flexGrow: 1 }}>
+                <AppBar className={styles.Appbar} style={{ backgroundColor: 'rgb(22, 22, 22)' }}>
+                    <Toolbar >
+                        <FaTh />
 
 
-                    <FaTrello className={styles.icon} />
-                    <h3 className={styles.heading}>Trello</h3>
+                        <ImHome className={styles.home} />
 
 
+                        {/* //SearchAppBar */}
+                        <div className={styles.searchbar}>
+                            <Search >
+                                <SearchIconWrapper>
+                                    <SearchIcon />
+                                </SearchIconWrapper>
+                                <StyledInputBase
+                                    placeholder="Search…"
+                                    inputProps={{ 'aria-label': 'search' }}
+                                />
+                            </Search>
+                        </div>
+                        <div className={styles.tag}>
+                            <FaTrello />
+                            <strong style={{ marginLeft: "4px" }}>Trello</strong>
 
-                    {/* //SearchAppBar */}
+                        </div>
+                        <div className={styles.notification}>
+                            <MdOutlineNotificationsNone />
+                            <RiQuestionLine style={{ marginLeft: "6px" }} />
+                            <MdBrightness6 style={{ marginLeft: "6px" }} />
+                        </div>
 
-                    <Search className={styles.searchbar}>
-                        <SearchIconWrapper>
-                            <SearchIcon />
-                        </SearchIconWrapper>
-                        <StyledInputBase
-                            placeholder="Search…"
-                            inputProps={{ 'aria-label': 'search' }}
-                        />
-                    </Search>
+                        <img className={styles.img} src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS9345jS1_3tkJK_gPIhK8tKcOiXy8XbNWi902Otr_6-Cb2uTQO-L5oDH1H-elFT-l_iI8&usqp=CAU" alt="userlogin" />
+                    </Toolbar>
+                </AppBar>
+                
+             </Box>
+         
+             <AppBar className={styles.transparentAppbar} style={{ backgroundColor: 'transparent' , marginTop : '3rem' , color:'black' }}>
+        <Toolbar>
+        <h4>Kanban</h4>
+        <FiStar className={styles.star}/>
 
-                    <MdOutlineNotificationsNone className={styles.notification}/>
-                    <RiQuestionLine className={styles.notification}/>
-                    <MdBrightness6 className={styles.notification}/>
-                </Toolbar>
-            </AppBar>
-        </Box>
+        <HiUsers  className={styles.users}/>
+        <h5>Workspace visible</h5>
+
+        <SiPowerbi className={styles.power}/>
+        <h5>Power-Ups</h5>
+        <AiTwotoneThunderbolt className={styles.Automation}/>
+        <h5>Automation</h5>
+
+        <BiFilter className={styles.filter}/>
+        <h5>Filter</h5>
+        <div className={styles.div}>
+        <RiUserAddLine/>
+        <h6 className={styles.share}>Share</h6>
+        </div>
+        <BsThreeDots className={styles.dots} />
+
+        <img className={styles.img} src="https://cdn-icons-png.flaticon.com/512/149/149071.png" alt= "userImg"/>
+        </Toolbar>
+      </AppBar>
+     
+</div>
+      
+
+
     );
 }
 
-// '&:hover': {
-//     backgroundColor: alpha(theme.palette.common.white, 0.25),
