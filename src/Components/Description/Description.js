92588f3a50318d7cdace9acf1341a4ba1470b1db
ROHAN-W11 <rohanrathi333@gmail.com>
2023-06-09 02:41:46 +0530

    Feat- Two feature added for Editmenu and Homenavbar. A  bug fix in tinymce

--- src/Components/Description/Description.js
+++ src/Components/Description/Description.js
@@ -51,8 +51,16 @@ const Description = () => {
                             init={{
                                 height: 300,
                                 menubar: false,
-                                plugins: ['paste', 'link'],
-                                toolbar: 'undo redo | bold italic | alignleft aligncenter alignright | link',
+                                plugins: [
+                                'advlist autolink lists link image charmap print preview anchor',
+                                'searchreplace visualblocks code fullscreen',
+                                'insertdatetime media table paste code help wordcount',
+                                ],
+                                toolbar:
+                                'undo redo | formatselect | ' +
+                                'bold italic backcolor | alignleft aligncenter ' +
+                                'alignright alignjustify | bullist numlist outdent indent | ' +
+                                'removeformat | help',
                             }}
                             onEditorChange={handleEditorChange}
                         />
