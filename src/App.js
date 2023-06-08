37aa52888071c13c0d6d04fcfff7a0854ff8ec17
bae151 <priyalsoni46@gmail.com>
2023-06-08 12:42:27 +0530

    kanban home page

--- src/App.js
+++ src/App.js
@@ -1,12 +1,10 @@
 import Home from "./Components/Home/Home";
-// import TodoList from "./Components/TodoList/TodoList";
-// import styles from './App.module.css';
 
 
 function App() {
   return (
     <div >
-      {/* <TodoList/> */}
+     
       <Home/>
     </div>
   );
