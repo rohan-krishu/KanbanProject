cb9c950c635d1c63e180e91c1487ec1ea1e60900
ashish@123456 <ashishkhankari0927@gmail.com>
2023-06-09 22:40:53 +0530

    Drag and Drop

--- src/Components/Redux/store.js
+++ src/Components/Redux/store.js
@@ -1,13 +1,10 @@
 import { configureStore } from "@reduxjs/toolkit";
 import todoReducer from './TodoSlice';
-import cardReducer from './CardSlice';
 
 const store = configureStore({
     reducer:{
         todo: todoReducer,
-        card: cardReducer
     }
 });
-
 
 export default store;
