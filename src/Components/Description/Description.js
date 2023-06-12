46cdb7a282c89356cec67f37f828b1d6b14e6c73
ROHAN-W11 <rohanrathi333@gmail.com>
2023-06-12 11:57:09 +0530

    fix - a bug fix in description box

--- src/Components/Description/Description.js
+++ src/Components/Description/Description.js
@@ -8,7 +8,6 @@ import { useNavigate, useParams } from 'react-router-dom';
 import { useSelector } from 'react-redux';
 import './Description.css'
 import { CgClose } from 'react-icons/cg';
-import ButtonGroup from '@mui/material/ButtonGroup';
 
 const Description = () => {
     const [content, setContent] = useState('<p>Description:</p>');
@@ -27,125 +26,42 @@ const Description = () => {
     const HandleClose = () => {
         Navigate("/kanban")
     }
-    
 
     return (
-        <div className='main-container'>
-            <CgClose
-                onClick={ HandleClose }
-            />
-            <div className='container'>
+        <div>
+            <div>
                 <Grid container spacing={2}>
                     <Grid item xs={12} md={6}>
                         <Box p={2} width="100%" mt={2}>
-                            <div style={{ marginTop: '50px' }}>
+                            <div style={{ marginTop: '50px', paddingBottom: '50px' }}>
                                 {/* <TextField id="outlined-basic" label="Section A" variant="outlined" fullWidth /> */}
                                 <Button variant="outlined" startIcon={<RemoveRedEyeIcon />} href="#outlined-buttons" sx={{ mt: 2 }}>
                                     Add
                                 </Button>
                             </div>
-
-                            <div style={{ marginTop: '60px' }}>
-                                <Editor
-                                    apiKey="vzto6l8svuc4whex2azy8pkvhl5vb3wr11y4tys3y7jwo686"
-                                    initialValue={content}
-                                    init={{
-                                        height: 300,
-                                        menubar: false,
-                                        plugins: ['paste', 'link'],
-                                        toolbar: 'undo redo | bold italic | alignleft aligncenter alignright | link',
-                                    }}
-                                    onEditorChange={handleEditorChange}
-                                />
-                            </div>
-
+                            <Editor 
+                                value={content}
+                                onEditorChange={handleEditorChange}
+                                apiKey="vzto6l8svuc4whex2azy8pkvhl5vb3wr11y4tys3y7jwo686"
+                            />
                             <div style={{ display: 'flex', alignItems: 'center', marginTop: '70px', marginBottom: '2rem' }}>
                                 <AccountCircleIcon />
                                 <TextField id="outlined-basic" label="Outlined" variant="outlined" fullWidth sx={{ marginLeft: 2 }} />
                             </div>
-
-                            
                         </Box>
                     </Grid>
                     <Grid item xs={12} md={6}>
-        <Box  p={2} width="400px" mt={2} textAlign="center">
-            <div>
-            <div>
-                <h3>Add to card</h3>
-                <ButtonGroup orientation="vertical" aria-label="vertical outlined button group">
-                <Button>
-                    <RemoveRedEyeIcon />
-                    Button 1
-                </Button>
-                <Button>
-                    <RemoveRedEyeIcon />
-                    Button 2
-                </Button>
-                <Button>
-                    <RemoveRedEyeIcon />
-                    Button 3
-                </Button>
-                <Button>
-                    <RemoveRedEyeIcon />
-                    Button 4
-                </Button>
-                <Button>
-                    <RemoveRedEyeIcon />
-                    Button 5
-                </Button>
-                </ButtonGroup>
-            </div>
-            <div>
-                <h3>Power-Ups</h3>
-                <ButtonGroup orientation="vertical" aria-label="vertical outlined button group">
-                <Button>
-                    <RemoveRedEyeIcon />
-                    Button 1
-                </Button>
-                </ButtonGroup>
-            </div>
-            <div>
-                <h3>Automation</h3>
-                <ButtonGroup orientation="vertical" aria-label="vertical outlined button group">
-                <Button>
-                    <RemoveRedEyeIcon />
-                    Button 1
-                </Button>
-                </ButtonGroup>
-            </div>
-            <div>
-                <h3>Actions</h3>
-                <ButtonGroup orientation="vertical" aria-label="vertical outlined button group">
-                <Button>
-                    <RemoveRedEyeIcon />
-                    Button 1
-                </Button>
-                <Button>
-                    <RemoveRedEyeIcon />
-                    Button 2
-                </Button>
-                <Button>
-                    <RemoveRedEyeIcon />
-                    Button 3
-                </Button>
-                <Button>
-                    <RemoveRedEyeIcon />
-                    Button 4
-                </Button>
-                <Button>
-                    <RemoveRedEyeIcon />
-                    Button 5
-                </Button>
-                </ButtonGroup>
-            </div>
-            </div>
-        </Box>
-        </Grid>
+                        <Box p={2} width="10%" mt={2} textAlign="center">
+                        <div>
+                            <Button onClick={HandleClose}>
+                                <CgClose />
+                            </Button>
+                        </div>
+                        </Box>
+                    </Grid>
                 </Grid>
             </div>
-
         </div>
-
     );
 };
 
